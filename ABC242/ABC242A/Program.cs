028f4651c20ce6218a4c642fd0a4eb545829f50d
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-13 19:36:21 +0900

    add : ABC242 A

---
+++ ABC242/ABC242A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC242A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abcx = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            if (abcx[3] <= abcx[0])
+            {
+                Console.WriteLine(1);
+                return;
+            }
+
+            if (abcx[3] > abcx[1])
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var d = abcx[1] - abcx[0];
+
+            Console.WriteLine(abcx[2]/d);
+
+        }
+    }
+}
