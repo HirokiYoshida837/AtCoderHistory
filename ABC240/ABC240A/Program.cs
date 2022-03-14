1c229f0a72b307b3ea852e41018cc0d7f035df29
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-14 23:12:48 +0900

    # add : solved ABC 240 A,B,C

---
+++ ABC240/ABC240A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC240A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var a = ab[0];
+            var b = ab[1];
+
+            Console.WriteLine((Math.Abs(a - b) <= 1 || (b == 10 && a == 1)) ? "Yes" : "No");
+        }
+    }
+}
