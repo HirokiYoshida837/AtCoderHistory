cb73a6d118cb9f40ac372f5d8dab3536dcc49461
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-16 00:45:38 +0900

    # add : solved ABC238 A, B, C

---
+++ ABC238/ABC238A/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ABC238A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine("No");
+            }
+            else if (n == 3)
+            {
+                Console.WriteLine("No");
+            }
+            else if (n==4)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+
+        }
+    }
+}
---
+++ ABC238/ABC238B/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC238B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var aSum = new List<int>();
+            aSum.Add(0);
+            foreach (var item in a)
+            {
+                aSum.Add((aSum[^1] + item) % 360);
+            }
+
+            aSum.Add(360);
+            aSum = aSum.OrderBy(x => x).ToList();
+
+            var max = 0;
+            for (var i = 1; i < aSum.Count; i++)
+            {
+                max = Math.Max(max, aSum[i] - aSum[i - 1]);
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
