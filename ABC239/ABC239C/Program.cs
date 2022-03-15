df543ba553147675c6a53b7fea23dc4f5b3006aa
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-15 23:00:30 +0900

    # add : solved ABC239 C, D

---
+++ ABC239/ABC239C/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC239C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xyxy = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            (int X, int Y) p1 = (xyxy[0], xyxy[1]);
+            (int X, int Y) p2 = (xyxy[2], xyxy[3]);
+
+            var ans = false;
+            for (int i = (int) p1.X - 3; i <= p1.X + 3; i++)
+            {
+                for (int j = (int) p1.Y - 3; j <= p1.Y + 3; j++)
+                {
+                    // Console.WriteLine($"{i} {j}");
+
+                    var dist1 = calcDist(p1, (i, j));
+                    var dist2 = calcDist(p2, (i, j));
+
+                    if ((dist1  == Math.Sqrt(5)) && (dist2 == Math.Sqrt(5)))
+                    {
+                        // Console.WriteLine($"{i} {j}");
+                        ans = true;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(ans ? "Yes" : "No");
+        }
+
+        static double calcDist((int X, int Y) p1, (int X, int Y) p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
+    }
+}
---
+++ ABC239/ABC239D/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC239D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abcd = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var a = abcd[0];
+            var b = abcd[1];
+            var c = abcd[2];
+            var d = abcd[3];
+
+            // エラストテネスの篩で事前に計算しておく
+            var primes = new bool[210];
+            for (var i = 2; i < primes.Length; i++)
+            {
+                primes[i] = true;
+            }
+            for (int p = 2; p * p <= 200; p++)
+            {
+                if (primes[p])
+                {
+                    for (int i = p * p; i <= 200; i += p)
+                    {
+                        primes[i] = false;
+                    }
+                }
+            }
+
+
+            for (int i = a; i <= b; i++)
+            {
+                var flag = true;
+                for (int j = c; j <= d; j++)
+                {
+                    // i+j が一つでも素数であれば、 Aokiくんがかつ。 i+j が全部素数じゃないなら、Takahashiくんがかつ。
+                    flag = flag && !primes[i + j];
+                }
+                
+                // i+j が素数じゃないものしかなかった
+                if(flag)
+                {
+                    Console.WriteLine("Takahashi");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Aoki");
+        }
+    }
+}
