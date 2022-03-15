dd7643ad3f11a752f006786779f9479b02d55f8e
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-15 09:57:01 +0900

    # add : solved ABC239 A, B

---
+++ ABC239/ABC239A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ABC239A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = double.Parse(Console.ReadLine());
+
+            var sqrt = Math.Sqrt( h * (12800000.0 + h));
+
+            Console.WriteLine(sqrt);
+        }
+    }
+}
---
+++ ABC239/ABC239B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC239B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = long.Parse(Console.ReadLine());
+
+            if (x < 0 && x %10 != 0)
+            {
+                Console.WriteLine(x/10 -1);
+            }
+            else
+            {
+                Console.WriteLine(x/10);
+            }
+        }
+    }
+}
