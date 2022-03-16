6b530d06fd43486035ca817c6aea308332d19b30
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-16 23:23:07 +0900

    # fix : 別の人の回答を参考に実装

--- ABC237/ABC237C/Program.cs
+++ ABC237/ABC237C/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
 
 namespace ABC237C
 {
@@ -10,60 +9,14 @@ namespace ABC237C
         {
             var s = Console.ReadLine();
 
-            var l = 0;
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == 'a')
-                {
-                    l++;
-                }
-                else
-                {
-                    break;
-                }
-            }
+            var head = s.Length - s.TrimStart('a').Length;
+            var tail = s.Length - s.TrimEnd('a').Length;
 
-            var revS = new string(s.Reverse().ToArray());
+            var trim = s.Trim('a');
 
-            var r = 0;
-            for (var i = 0; i < revS.Length; i++)
-            {
-                if (revS[i] == 'a')
-                {
-                    r++;
-                }
-                else
-                {
-                    break;
-                }
-            }
+            var res = trim.Reverse().SequenceEqual(trim) && (head <= tail);
 
-            if (l > r)
-            {
-                Console.WriteLine("No");
-                return;
-            }
-
-            var sb = new StringBuilder().Append(revS);
-
-            for (int i = 0; i < r-l; i++)
-            {
-                sb.Append("a");
-            }
-            
-            var s1 = sb.ToString();
-
-            for (int i = 0; i < s1.Length; i++)
-            {
-                if (s1[i] != s1[s1.Length - 1 - i])
-                {
-                    Console.WriteLine("No");
-                    return;
-                }
-            }
-
-            Console.WriteLine("Yes");
-            
+            Console.WriteLine(res ? "Yes" : "No");
         }
     }
 }
