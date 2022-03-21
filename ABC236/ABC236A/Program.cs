8f91874dc3d2cd784e0e0565f36dc1e34233b8e3
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-21 22:53:44 +0900

    # add : solved ABC236 A, B, C, D

---
+++ ABC236/ABC236A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC236A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().ToCharArray();
+            var ab = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var (a, b) = (ab[0], ab[1]);
+
+            var c = s[a-1];
+
+            s[a-1] = s[b-1];
+            s[b-1] = c;
+
+            var s1 = new string(s);
+            Console.WriteLine(s1);
+        }
+    }
+}
