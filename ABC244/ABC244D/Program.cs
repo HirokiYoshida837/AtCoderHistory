fa22bf385449b49a6aa16216bc61f06eeb64153b
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-03-31 23:31:13 +0900

    # add : virtual contest ABC244

---
+++ ABC244/ABC244D/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC244D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split().ToArray();
+            var t = Console.ReadLine().Split().ToArray();
+
+            var count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (s[i] != t[i])
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
