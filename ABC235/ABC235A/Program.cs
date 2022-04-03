f423445f3ac3df3aba21e99958fe14354b9e2ad5
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-04-03 23:26:47 +0900

    # add : add ABC235

---
+++ ABC235/ABC235A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC235A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var abc = Console.ReadLine();
+            var bca = new string(new[] {abc[1], abc[2], abc[0]});
+            var cab = new string(new[] {abc[2], abc[0], abc[1],});
+
+            var sum = new[] {abc, bca, cab}.ToList()
+                .Select(int.Parse)
+                .ToList()
+                .Sum();
+
+            Console.WriteLine(sum);
+        }
+    }
+}
