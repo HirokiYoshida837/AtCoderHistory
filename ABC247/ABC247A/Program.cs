b0202fa44224fd93ccce47807b7a7015f69f3585
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-04-10 23:26:29 +0900

    # add : solved ABC247 A,B,C,D

---
+++ ABC247/ABC247A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC247A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            Console.Write("0");
+
+            for (var i = 0; i < s.Length-1; i++)
+            {
+                var c = s[i];
+                Console.Write(c);
+            }
+
+            Console.WriteLine();
+
+            
+        }
+    }
+}
---
+++ ABC247/ABC247B/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC247B
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var st = Enumerable.Range(0, n)
+                .Select(x => Console.ReadLine().Split().ToList())
+                // .Select(x => (x[0], x[1]))
+                .ToList();
+
+            for (int i = 0; i < n; i++)
+            {
+                var canGivenANickName = false;
+                
+                foreach (var s in st[i])
+                {
+                    var s_ok = true;
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (s == st[j][0] || s == st[j][1])
+                            {
+                                s_ok = false;
+                            }
+                        }
+                    }
+
+                    if (s_ok)
+                    {
+                        canGivenANickName = true;
+                    }
+                }
+
+                if (!canGivenANickName)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+            
+        }
+    }
+}
