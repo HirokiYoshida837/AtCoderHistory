6ca09780d67cd6281ac94eb1b9084d5a8691fbfd
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-04-13 23:05:50 +0900

    # add : solved AVC232A,B,D

---
+++ ABC232/ABC232A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC232A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var a = char.ToString(s[0]);
+            var b = char.ToString(s[^1]);
+
+            var an = int.Parse(a);
+            var bn = int.Parse(b);
+
+            Console.WriteLine(an*bn);
+        }
+    }
+}
---
+++ ABC232/ABC232B/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC232B
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine().ToCharArray();
+            var t = Console.ReadLine().ToCharArray();
+
+            var diff = new List<int>();
+            
+            for (var i = 0; i < s.Length; i++)
+            {
+                var sc = s[i];
+                var tc = t[i];
+
+                var d = tc - sc;
+
+                if (d<0)
+                {
+                    d += 26;
+                }
+                
+                diff.Add(d);
+            }
+
+            var hashSet = diff.ToHashSet();
+            Console.WriteLine(hashSet.Count == 1 ? "Yes" : "No");
+        }
+    }
+}
