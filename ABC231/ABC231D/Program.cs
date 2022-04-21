18e0ef723a825ba270b969c14580bc080a5683af
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-04-22 00:38:43 +0900

    # fix : UnionFindの実装を追加、修正

--- ABC231/ABC231D/Program.cs
+++ ABC231/ABC231D/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -63,18 +64,22 @@ namespace ABC231D
         }
     }
 
-    public class UnionFind
+        public class UnionFind
     {
-        public int Size { get; private set; }
-        public int GroupCount { get; private set; }
+        private int Size { get; set; }
+        private int GroupCount { get; set; }
 
         private int[] Parent;
         private int[] Sizes;
 
+        /// <summary>
+        /// n要素のUnionFind-Treeを生成する
+        /// </summary>
+        /// <param name="count"></param>
         public UnionFind(int count)
         {
-            this.Size = count;
-            this.GroupCount = count;
+            Size = count;
+            GroupCount = count;
 
             Parent = new int[count];
             Sizes = new int[count];
@@ -88,29 +93,56 @@ namespace ABC231D
 
         public bool TryUnite(int x, int y)
         {
-            var xp = Find(x);
-            var yp = Find(y);
+            return TryUnite(x, y, out var p);
+        }
+
+        /// <summary>
+        /// xとyの属している木を合併する。<br/>
+        /// xとyがすでに同じ木にある場合、falseを返す。
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="parent"> xとyの合併後のroot </param>
+        /// <returns></returns>
+        public bool TryUnite(int x, int y, out int parent)
+        {
+            //       ans += map.TryGetValue(cum[r] - k, out var val) ? val : 0;
+
+            var xp = FindRoot(x);
+            var yp = FindRoot(y);
+            // rootが同じであれば、すでにUnite済なのでエラー。
             if (xp == yp)
             {
+                parent = xp;
                 return false;
             }
 
             if (Sizes[xp] < Sizes[yp])
             {
-                var tmp = xp;
-                xp = yp;
-                yp = tmp;
+                (yp, xp) = (xp, yp);
             }
 
             GroupCount--;
 
             Parent[yp] = xp;
             Sizes[xp] += Sizes[yp];
+
+            parent = xp;
             return true;
         }
 
-        public int Find(int x)
+        /// <summary>
+        /// xのrootを返す
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int FindRoot(int x)
         {
+            // if (x == Parent[x]) return x;
+            // return FindRoot(Parent[x]);
+            
+            // 再帰しないので速そう
             while (x != Parent[x])
             {
                 x = (Parent[x] = Parent[Parent[x]]);
@@ -119,14 +151,25 @@ namespace ABC231D
             return x;
         }
 
+        /// <summary>
+        /// rootになっているノードをすべて返す
+        /// </summary>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public IEnumerable<int> AllRepresents()
         {
             return Parent.Where((x, y) => x == y);
         }
 
+        /// <summary>
+        /// xが属している木のサイズを返す
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetSize(int x)
         {
-            return Sizes[Find(x)];
+            return Sizes[FindRoot(x)];
         }
     }
 }
