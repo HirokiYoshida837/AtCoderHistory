bcd2b76077835b3b6923b699a527e48c87346304
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-04-22 00:40:25 +0900

    # add : ReadValue関連を追加

---
+++ library/ABCUtils/IOUtils/ReadValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ABCUtils.IOUtils
+{
+    public class ReadValues
+    {
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+    }
+}
