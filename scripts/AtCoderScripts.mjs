e738836ca34501f363ed2cc54fc0e18378f6560e
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-08 16:30:56 +0900

    # add : zxスクリプトを整理

---
+++ scripts/AtCoderScripts.mjs
@@ -0,0 +1,22 @@
+﻿export async function createProj(ProjName, needTest) {
+    
+    console.log("--- --- --- create Main project --- --- ---")
+    let mainCreateCode = await $`dotnet new AtCoderMain -o "${ProjName}"`
+
+    if (needTest) {
+
+        console.log("--- --- --- create Test project --- --- ---")
+        let testCreateCode = await $`dotnet new AtCoderTest -o "${ProjName}".Test`
+
+        return await Promise.all([mainCreateCode, testCreateCode])
+            .then(async res => {
+                // 何故か文字化けする。
+                await $`dotnet add "${ProjName}".Test/"${ProjName}".Test.csproj reference "${ProjName}"/"${ProjName}".csproj`
+                await $`dotnet remove "${ProjName}".Test/"${ProjName}".Test.csproj reference "..\ABCXXX\ABCXXX.csproj"`
+            })
+
+    } else {
+        return mainCreateCode;
+    }
+
+}
