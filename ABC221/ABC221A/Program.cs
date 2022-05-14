49140d3ca34f5ec03382f02ccd3dfb9535ba56c2
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-14 21:53:45 +0900

    # add : solved ABC221 A,B,C

---
+++ ABC221/ABC221A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ABC221A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var (a,b) = ReadValue<int,int>();
+            var diff = a - b;
+            Console.WriteLine(Math.Pow(32,diff));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+        
+    }
+}
---
+++ ABC221/ABC221B/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC221B
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = ReadValue<String>();
+            var t = ReadValue<String>();
+
+            var ans = false;
+
+            if (s == t)
+            {
+                Console.WriteLine("Yes");
+                return;
+            }
+
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                var tmp = s.ToArray();
+                var tc = tmp[i];
+                tmp[i] = tmp[i - 1];
+                tmp[i - 1] = tc;
+
+                if (new string(tmp) == t)
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+
+            Console.WriteLine("No");
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+    }
+}
