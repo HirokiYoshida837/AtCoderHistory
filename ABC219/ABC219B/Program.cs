7c78372b71112cbd1fae43ef2a4cc1997ef07510
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-15 15:16:24 +0900

    # add : solved ABC219A,B,C

---
+++ ABC219/ABC219B/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC219B
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s1 = ReadValue<string>();
+            var s2 = ReadValue<string>();
+            var s3 = ReadValue<string>();
+
+            var s = new[] {s1, s2, s3};
+
+            var t = ReadValue<string>().ToCharArray().Select(x => x - '0').ToArray();
+
+            var sb = new StringBuilder();
+            foreach (var i in t)
+            {
+                sb.Append(s[i - 1]);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
