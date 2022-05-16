5d2b21ef49be81a29f683dc9d720f69fa9b8a5ff
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-16 23:42:31 +0900

    # add : solved ABC217

---
+++ ABC217/ABC217C/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC217C
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = ReadValue<long>();
+            var p = ReadList<long>().ToArray();
+
+            var q = new long[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                var pi = p[i - 1];
+                q[pi - 1] = i;
+            }
+
+            var joined = string.Join(" ", q);
+            Console.WriteLine(joined);
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+        
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
