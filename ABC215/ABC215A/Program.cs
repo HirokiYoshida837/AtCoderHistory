d6e756f6626fdd6adf6cefa6ab758c7ff85d09f3
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-18 00:00:32 +0900

    # add : solved ABC215A,B,C

---
+++ ABC215/ABC215A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC215A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine() == "Hello,World!" ? "AC" : "WA");
+        }
+    }
+}
---
+++ ABC215/ABC215B/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC215B
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var ans = 2L;
+            var count = 0L;
+            while (ans <= n)
+            {
+                ans <<= 1;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
