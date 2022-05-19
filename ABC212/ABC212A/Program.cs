508f8875977e56d588b2d7b5c345acbc1c987264
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-19 22:34:30 +0900

    # add : solved ABC 212-A,B,C, 213-A,B, 214-A,B

---
+++ ABC212/ABC212A/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC212A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var (a, b) = ReadValue<int, int>();
+
+            if (0 < a && b == 0)
+            {
+                Console.WriteLine("Gold");
+            }
+            else if (a == 0 && 0 < b)
+            {
+                Console.WriteLine("Silver");
+            }
+            else
+            {
+                Console.WriteLine("Alloy");
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
---
+++ ABC213/ABC213A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ABC213A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var (a, b) = ReadValue<int, int>();
+
+            Console.WriteLine(b^a);
+            
+            // 全探索するとこうなる
+            // for (int c = 0; c <=255; c++)
+            // {
+            //     if ((a^c) == b)
+            //     {
+            //         Console.WriteLine(c);
+            //         return;
+            //     }
+            // }
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+    }
+}
