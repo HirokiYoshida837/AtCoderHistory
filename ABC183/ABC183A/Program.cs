52169be705c1d9308b3a045d6734a629472599d1
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-05-24 23:33:15 +0900

    add : solved ABC183A,B

---
+++ ABC183/ABC183A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ABC183A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Max(x, 0));
+        }
+    }
+}
