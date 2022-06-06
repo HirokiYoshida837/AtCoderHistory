b12e3c617cece702d41818803d16d5a867fbd582
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-06-06 21:17:24 +0900

    add :solved abc 178A,B,C

---
+++ ABC178/ABC178A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC178A
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1 - int.Parse(Console.ReadLine()));
+        }
+    }
+}
