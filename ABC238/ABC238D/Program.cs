ba215c79e1ce2b4868ffda7c8e3221cdb354d5ef
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-06-12 17:03:36 +0900

    add : solved ABC238 D

---
+++ ABC238/ABC238D/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC238D
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t = ReadValue<long>();
+            for (int i = 0; i < t; i++)
+            {
+                var (a, s) = ReadValue<long, long>();
+                
+                // x + y == (x xor y) + (x and y) << 2 で表せる (全加算器)
+
+                if (s - 2 * a < 0)
+                {
+                    Console.WriteLine("No");
+                    continue;
+                }
+
+
+                var l = (s - 2 * a) & a;
+                if (l == 0)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+
+            // Console.WriteLine(ans);
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
