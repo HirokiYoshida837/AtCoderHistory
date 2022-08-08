a6130c2fcaaef324271202966cc5075f2b662f82
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-08-08 23:24:41 +0900

    add : solved EDCP-I

---
+++ EDCP/EDCP-I/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace EDCP_I
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = ReadValue<int>();
+            var p = ReadList<double>().ToArray();
+
+
+            var dp = new double[n + 1, n + 1];
+            dp[0, 0] = 1;
+            
+            // i : i枚目までなげる
+            // j : 表がでた数
+            // dp[i,j] : i枚目までなげて、j枚表がでたときの確率。
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (dp[i, j] == 0)
+                    {
+                        continue;
+                    }
+
+                    // 裏が出たとき
+                    dp[i + 1, j] += dp[i, j] * (1 - p[i]);
+                    // 表がでたとき
+                    dp[i + 1, j + 1] += dp[i, j] * (p[i]);
+                }
+            }
+
+
+            var sum = 0.0d;
+            for (int i = (n + 1) / 2; i < n + 1; i++)
+            {
+                sum += dp[n,i];
+            }
+
+            Console.WriteLine(sum);
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
