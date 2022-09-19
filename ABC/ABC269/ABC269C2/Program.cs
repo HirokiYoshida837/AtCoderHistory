70cfc233538bf2e09a85a28daf35d8337098a541
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-09-19 20:47:17 +0900

    add : solved ABC269 A,B,C (upsolve)

---
+++ ABC/ABC269/ABC269C2/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC269C2
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = ReadValue<long>();
+
+            var bitS = Convert.ToString(n, 2).PadLeft(61, '0');
+
+            var valueTuples = bitS.Reverse().Select((x, i) => (x, i)).Where(x => x.x == '1').Select(x => x.i).ToArray();
+
+            var count = valueTuples.Length;
+
+            var ansList = new List<long>() {0};
+
+
+            for (int i = 1; i <= count; i++)
+            {
+                var permutations = new Permutations(i);
+
+                foreach (var ints in permutations.NCR(valueTuples))
+                {
+                    var ans = 0L;
+                    foreach (var i1 in ints)
+                    {
+                        var v = 1L << i1;
+                        ans += v;
+                    }
+
+                    ansList.Add(ans);
+                }
+            }
+
+
+            foreach (var l in ansList.Distinct().OrderBy(x => x))
+            {
+                Console.WriteLine(l);
+            }
+        }
+
+        /// <summary>
+        /// 順列の中からr個を取得。重複なし。(これCombinationだから名前間違ってるね)
+        /// 
+        /// <example>
+        /// <code>
+        /// var items = Enumerable.Range(1, 5).Select(x => x).ToArray();
+        /// var perm = new PermutationS(3);
+        /// foreach (var ints in permutations.NCR(items))
+        /// {
+        ///     var @join = String.Join(' ', ints);
+        ///     Console.WriteLine(join);
+        /// }
+        ///
+        /// // results
+        /// 1 2 3
+        /// 1 2 4
+        /// 1 2 5
+        /// 1 3 4
+        /// 1 3 5
+        /// 1 4 5
+        /// 2 3 4
+        /// 2 3 5
+        /// 2 4 5
+        /// 3 4 5
+        /// </code>
+        /// </example>
+        /// </summary>
+        public class Permutations
+        {
+            private int r;
+
+            public Permutations(int r)
+            {
+                this.r = r;
+            }
+
+            // 順列の中から順にr個選ぶ。
+            public IEnumerable<T[]> NCR<T>(T[] items)
+            {
+                if (items.Count() == 1)
+                {
+                    yield return new T[] {items.First()};
+                    yield break;
+                }
+
+                var n = items.Count();
+
+                for (int bit = 0; bit < 1 << n; bit++)
+                {
+                    var bitS = Convert.ToString(bit, 2).PadLeft(n, '0');
+                    if (bitS.Count(x => x == '0') != r) continue;
+
+                    var A = new List<T>();
+
+                    for (var i = 0; i < bitS.Length; i++)
+                    {
+                        var c = bitS[i];
+                        if (c == '0') A.Add(items[i]);
+                    }
+
+                    yield return A.ToArray();
+                }
+
+                yield break;
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
---
+++ ABC/ABC269/ABC269D2/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ABC269D2
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = ReadValue<int>();
+            var xyList = Enumerable.Range(0, n)
+                .Select(_ => ReadValue<int, int>())
+                .ToArray();
+
+            var dictionary = xyList.Select((item, index) => (item, index)).ToDictionary(x => x.item, x => x.index);
+
+            var dir = new (int dx, int dy)[]
+            {
+                (-1, -1), (-1, 0), (0, -1), (0, +1), (1, 0), (1, 1)
+            };
+
+            var uf = new UnionFind(n + 10);
+
+            foreach (var kv in dictionary)
+            {
+                var (x, y) = kv.Key;
+
+                foreach (var (dx, dy) in dir)
+                {
+                    var t = (x + dx, y + dy);
+                    if (dictionary.ContainsKey(t))
+                    {
+                        var ti = dictionary[t];
+                        uf.TryUnite(kv.Value, ti);
+                    }
+                }
+            }
+
+
+            var ansSet = new HashSet<int>();
+            foreach (var kv in dictionary)
+            {
+                var findRoot = uf.FindRoot(kv.Value);
+                ansSet.Add(findRoot);
+            }
+
+            Console.WriteLine(ansSet.Count);
+        }
+
+        public class UnionFind
+        {
+            private int Size { get; set; }
+            private int GroupCount { get; set; }
+
+            private int[] Parent;
+            private int[] Sizes;
+
+            /// <summary>
+            /// n要素のUnionFind-Treeを生成する
+            /// </summary>
+            /// <param name="count"></param>
+            public UnionFind(int count)
+            {
+                Size = count;
+                GroupCount = count;
+
+                Parent = new int[count];
+                Sizes = new int[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    Parent[i] = i;
+                    Sizes[i] = 1;
+                }
+            }
+
+            public bool TryUnite(int x, int y)
+            {
+                return TryUnite(x, y, out var p);
+            }
+
+            /// <summary>
+            /// xとyの属している木を合併する。<br/>
+            /// xとyがすでに同じ木にある場合、falseを返す。
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <param name="parent"> xとyの合併後のroot </param>
+            /// <returns></returns>
+            public bool TryUnite(int x, int y, out int parent)
+            {
+                //       ans += map.TryGetValue(cum[r] - k, out var val) ? val : 0;
+
+                var xp = FindRoot(x);
+                var yp = FindRoot(y);
+                // rootが同じであれば、すでにUnite済なのでエラー。
+                if (xp == yp)
+                {
+                    parent = xp;
+                    return false;
+                }
+
+                if (Sizes[xp] < Sizes[yp])
+                {
+                    (yp, xp) = (xp, yp);
+                }
+
+                GroupCount--;
+
+                Parent[yp] = xp;
+                Sizes[xp] += Sizes[yp];
+
+                parent = xp;
+                return true;
+            }
+
+            /// <summary>
+            /// xのrootを返す
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int FindRoot(int x)
+            {
+                // if (x == Parent[x]) return x;
+                // return FindRoot(Parent[x]);
+
+                // 再帰しないので速そう
+                while (x != Parent[x])
+                {
+                    x = (Parent[x] = Parent[Parent[x]]);
+                }
+
+                return x;
+            }
+
+            /// <summary>
+            /// rootになっているノードをすべて返す
+            /// </summary>
+            /// <returns></returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public IEnumerable<int> AllRepresents()
+            {
+                return Parent.Where((x, y) => x == y);
+            }
+
+            /// <summary>
+            /// xが属している木のサイズを返す
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetSize(int x)
+            {
+                return Sizes[FindRoot(x)];
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
