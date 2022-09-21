a8b4268f19ab0dfe69e66afdf406298c0cef8723
h.yoshida <hiroki.yoshida01@outlook.jp>
2022-09-21 21:29:22 +0900

    add : solved BootCamp Hard 030(upsolve with editorial)

---
+++ BootCampForBeginners/Hard/Hard_030_2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Hard_030_2
+{
+    /// <summary>
+    /// [A - Getting Difference](https://atcoder.jp/contests/agc018/tasks/agc018_a)
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var (n, k) = ReadValue<int, long>();
+            var aList = ReadList<long>().Distinct().ToList();
+
+            var aMax = aList.Max();
+
+            if (k > aMax)
+            {
+                Console.WriteLine("IMPOSSIBLE");
+                return;
+            }
+
+            var gcd = aList.Aggregate((x,acc) => GCD(x,acc));
+            if (k % gcd != 0)
+            {
+                Console.WriteLine("IMPOSSIBLE");
+            }
+            else
+            {
+                Console.WriteLine("POSSIBLE");
+            }
+        }
+        
+        /// <summary>
+        /// 最大公約数 (the Greatest Common Divisor) を計算します。(再帰なし)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static long GCD(long a, long b)
+        {
+            while (true)
+            {
+                if (b == 0) return a;
+                a %= b;
+                if (a == 0) return b;
+                b %= a;
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
