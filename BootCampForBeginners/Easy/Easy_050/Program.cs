e0e6b64300245ac459e63aed6283c18fe117d1ac
h.yoshida <hiroki.yoshida01@outlook.jp>
2023-02-28 19:45:06 +0900

    add : solved BootCamp(Easy) 042, 043, 044, 046, 049, 050, 053, 058, 059

---
+++ BootCampForBeginners/Easy/Easy_050/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Easy_050
+{
+    // https://atcoder.jp/contests/abc059/tasks/abc059_b
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = ReadValue<string>();
+            var b = ReadValue<string>();
+
+            if (a.Length > b.Length)
+            {
+                Console.WriteLine("GREATER");
+            }
+            else if (a.Length < b.Length)
+            {
+                Console.WriteLine("LESS");
+            }
+            else
+            {
+                if (a == b)
+                {
+                    Console.WriteLine("EQUAL");
+                    return;
+                }
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] - '0' > b[i] - '0')
+                    {
+                        Console.WriteLine("GREATER");
+                        return;
+                    }
+                    else if (a[i] - '0' < b[i] - '0')
+                    {
+                        Console.WriteLine("LESS");
+                        return;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
---
+++ BootCampForBeginners/Easy/Easy_059/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Easy_059
+{
+    // https://atcoder.jp/contests/abc104/tasks/abc104_b
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = ReadValue<string>();
+
+            bool check1()
+            {
+                return s[0] == 'A';
+            }
+
+            bool check2()
+            {
+                var count = 0;
+                for (var i = 2; i < s.Length - 1; i++)
+                {
+                    if (s[i] == 'C')
+                    {
+                        count++;
+                    }
+                }
+
+                return count == 1;
+            }
+
+            bool check3()
+            {
+                var count = s.Count(x => x >= 'a' && x <= 'z');
+
+                if (count != s.Length - 2)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            if (check1() && check2() && check3())
+            {
+                Console.WriteLine("AC");
+            }
+            else
+            {
+                Console.WriteLine("WA");
+            }
+        }
+
+
+        public static T ReadValue<T>()
+        {
+            var input = Console.ReadLine();
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+
+        public static (T1, T2) ReadValue<T1, T2>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2))
+            );
+        }
+
+        public static (T1, T2, T3) ReadValue<T1, T2, T3>()
+        {
+            var input = Console.ReadLine().Split();
+            return (
+                (T1) Convert.ChangeType(input[0], typeof(T1)),
+                (T2) Convert.ChangeType(input[1], typeof(T2)),
+                (T3) Convert.ChangeType(input[2], typeof(T3))
+            );
+        }
+
+        /// <summary>
+        /// 指定した型として、一行読み込む。
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+#nullable enable
+        public static IEnumerable<T> ReadList<T>(params char[]? separator)
+        {
+            return Console.ReadLine()
+                .Split(separator)
+                .Select(x => (T) Convert.ChangeType(x, typeof(T)));
+        }
+#nullable disable
+    }
+}
